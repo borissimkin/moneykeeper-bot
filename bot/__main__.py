985d34b4673d1695142fa616f6b7d1f35e694508
Boris <43819225+borissimkin@users.noreply.github.com>
2020-01-26 11:39:11 +0300

    Сделан шедулер бэкапа бд

--- bot/__main__.py
+++ bot/__main__.py
@@ -2,6 +2,7 @@ import datetime
 import logging
 
 from telegram.ext import CommandHandler, CallbackQueryHandler
+from telegram.ext.jobqueue import Days
 
 from bot import dispatcher, updater, jobs, config, logger
 from bot.commands.start import StartHandler
@@ -12,7 +13,7 @@ from bot.conversations.add_earning.handlers import add_earning
 from bot.conversations.edit_categories.handlers import edit_categories
 from bot.conversations.delete_transaction.handlers import delete_transaction
 from bot.conversations.view_transactions.query_handlers import view_transactions, handler_view_transactions
-from bot.job_queue.handlers import job_results
+from bot.job_queue.handlers import job_results, job_backup_database
 
 
 def start_handlers():
@@ -38,6 +39,10 @@ def start_handlers():
     jobs.run_daily(callback=job_results,
                    time=datetime.datetime.strptime(
                        config['jobs']['results_time'], '%H:%M:%S').time())
+
+    jobs.run_daily(callback=job_backup_database,
+                   time=datetime.datetime.strptime(
+                       config['jobs']['backup_database'], '%H:%M:%S').time())
     logger.info("All handlers successfully launched!")
 
 
